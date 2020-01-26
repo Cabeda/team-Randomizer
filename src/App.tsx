44d4fa78e619a810585fb222f27d396a28c3b84a
José Cabeda <jecabeda@gmail.com>
2020-01-26 01:29:42 +0000

    Refactor of code to use react hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,50 @@
-import * as React from 'react';
-import { Imatch } from './Shared/match.interface';
-import { IRandomizer } from './Shared/random.interface';
-import './App.css';
-import logo from './ball.svg';
+import React, { useState } from 'react';
+import { Imatch } from "./Shared/match.interface";
+import "./App.css";
 
+function App() {
+  const [players, setPlayers] = useState<String[]>([])
+  const [teams, setTeams] = useState<String[]>([])
+  const [result, setResult] = useState<Imatch[]>([])
 
-class App extends React.Component<any, IRandomizer> {
+  const handleTeamsChange = (e: React.FormEvent<HTMLTextAreaElement>) => setTeams(e.currentTarget.value.split(","))
 
+  const handlePlayersChange = (e: React.FormEvent<HTMLTextAreaElement>) => setPlayers(e.currentTarget.value.split(","))
 
-  public players: string[] = ["Moura", "Cabeda", "Ricardo", "Ivan", "André Sousa", "Simão", "João", "José Pedro"];
-
-  public teams: string[] = ["Egypt", "Morocco",
-    "Nigeria", "Senegal", "Tunisia", "Australia",
-    "Iran",
-    "Japan",
-    "Korea Republic",
-    "Saudi Arabia",
-    "Belgium",
-    "Croatia",
-    "Denmark",
-    "England",
-    "France",
-    "Germany",
-    "Iceland",
-    "Poland",
-    "Portugal",
-    "Russia",
-    "Serbia",
-    "Spain",
-    "Sweden",
-    "Switzerland",
-    "Costa Rica",
-    "Mexico",
-    "Panama",
-    "Argentina",
-    "Brazil",
-    "Colombia",
-    "Peru",
-    "Uruguay"];
-
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      players: this.printData(this.players),
-      result: [],
-      teams: this.printData(this.teams)
-    };
-
-    this.handleTeamsChange = this.handleTeamsChange.bind(this);
-    this.handlePlayersChange = this.handlePlayersChange.bind(this);
-    this.randomize = this.randomize.bind(this);
-  }
-
-  public handleTeamsChange(event: React.FormEvent<HTMLTextAreaElement>) {
-    this.setState({ teams: this.getItemsFromTextArea(event.currentTarget.value) });
-  }
-
-  public handlePlayersChange(event: React.FormEvent<HTMLTextAreaElement>) {
-    this.setState({ players: this.getItemsFromTextArea(event.currentTarget.value) });
-  }
-
-  public getItemsFromTextArea(text: string): string[] {
-    return text.split(",");
-  }
-
-  public printResults(list: Imatch[]): string {
+  const printResults = (list: Imatch[]): string => {
     let result: string = "";
     for (const item of list) {
       result += "\n" + item.player + " - " + item.team;
-    };
+    }
     return result;
   }
 
-
-  public printData(list: string[]): string[] {
-    return list.map(x => "\n" + x.trim() );
-
+  const printData = (list: String[]): string[] => {
+    return list.map(x => "\n" + x.trim());
   }
 
-  public getItemFromList(list: string[]) {
-    const item = list.splice(Math.floor((Math.random() * list.length)), 1)[0];
+  const getItemFromList = (list: string[]) => {
+    const item = list.splice(Math.floor(Math.random() * list.length), 1)[0];
     return item.trim();
   }
 
-
-  public randomize() {
+  const randomize = () => {
     const matches: Imatch[] = [];
-    const playersLength = this.state.players.length;
-    const playersToAdd = JSON.parse(JSON.stringify(this.state.players));
-    const teamsToAdd = JSON.parse(JSON.stringify(this.state.teams));
+    const playersLength = players.length;
+    const playersToAdd = JSON.parse(JSON.stringify(players));
+    const teamsToAdd = JSON.parse(JSON.stringify(teams));
 
     for (let i = 0; i < playersLength; i++) {
-      const player: string = this.getItemFromList(playersToAdd);
-      const team: string = this.getItemFromList(teamsToAdd);
+      const player: string = getItemFromList(playersToAdd);
+      const team: string = getItemFromList(teamsToAdd);
       const newTeam: Imatch = { player, team };
 
       matches.push(newTeam);
     }
 
-    this.setState({teams: this.printData(this.state.teams), players: this.printData(this.state.players), result: matches });
+      setResult(matches); 
   }
 
-  public render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -109,25 +52,39 @@ class App extends React.Component<any, IRandomizer> {
         </header>
         <div>
           <div className="container">
-          <div className="cell">
-          <h4>Players</h4>
-            <textarea id="players" placeholder="Insert players here" value={this.state.players} onChange={this.handlePlayersChange} />
+            <label>
+              Mode
+            </label>
           </div>
-          <div className="cell">
-          <h4>Teams</h4>
-            <textarea id="teams" placeholder="Insert teams here" value={this.state.teams} onChange={this.handleTeamsChange} />
+          <div className="container">
+            <div className="cell">
+              <h4>Players</h4>
+              <textarea
+                id="players"
+                placeholder="Insert players separated by a comma here"
+                value={printData(players)}
+                onChange={handlePlayersChange}
+              />
+            </div>
+            <div className="cell">
+              <h4>Teams</h4>
+              <textarea
+                id="teams"
+                placeholder="Insert teams separated by a comma here"
+                value={printData(teams)}
+                onChange={handleTeamsChange}
+              />
             </div>
           </div>
-          <button id="randomizeBtn" className="btn" onClick={this.randomize}>
-            <h2 >Randomize!!!</h2>
+          <button id="randomizeBtn" className="btn" onClick={randomize}>
+            <h2>Randomize!!!</h2>
           </button>
 
           <h4>Result</h4>
-          <textarea id="result" value={this.printResults(this.state.result)} />
+          <textarea id="result" value={printResults(result)} />
         </div>
       </div>
     );
-  }
 }
 
 export default App;
--- src/Shared/random.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Imatch } from "./match.interface";
-
-export interface IRandomizer {
-    players: string[];
-    teams: string[];
-    result: Imatch[]
-}
