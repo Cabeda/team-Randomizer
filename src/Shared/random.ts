dfa2283658fd9e73c6642edc0e7157a93f691caf
José Cabeda <jose.cabeda@fidel.uk>
2023-02-21 21:12:22 +0000

    Fix randomizer

--- src/Shared/random.ts
+++ src/Shared/random.ts
@@ -1,24 +1,18 @@
 import { Imatch } from "./match.interface";
 
-function getItemFromList(list: string[]): string {
-  const item = list.splice(Math.floor(Math.random() * list.length), 1)[0];
-  return item.trim();
-}
-
 export function Randomize(players: any[], teams: any[]): Imatch[] {
   const matches: Imatch[] = [];
 
-  if (players.length === 0 || teams.length === 0)
-    throw Error("You need at least one player and one team");
+  const shuffledPlayers = players.sort((x) => 0.5 - Math.random());
+  const shuffledTeams = teams.sort((x) => 0.5 - Math.random());
 
-  if (teams.length < players.length) {
-    throw Error("For this mode you need at least an equal number of teams");
-  }
-  while (players.length > 0) {
-    const player = getItemFromList(players);
-    const team = getItemFromList(teams);
-    const newTeam: Imatch = { player, team };
-
+  for (let i = 0; i < shuffledPlayers.length; i++) {
+    const newTeam: Imatch = {
+      player: shuffledPlayers[i],
+      team: shuffledTeams[
+        i - Math.floor(i / shuffledTeams.length) * shuffledTeams.length
+      ],
+    };
     matches.push(newTeam);
   }
 
