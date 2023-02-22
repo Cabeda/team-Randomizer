468b7fd09037ffda3790a81cfb59e6f16a3905ac
José Cabeda <jose.cabeda@fidel.uk>
2023-02-22 09:19:54 +0000

    Fix unit test

--- src/Shared/random.test.js
+++ src/Shared/random.test.js
@@ -1,9 +1,9 @@
-import {Randomize} from './random';
+import { Randomize } from "./random";
 
-const players = ["A", "B", "C", "D"]
-const teams = ["Braga", "Porto"]
-test('returns a match given a list', () => {
-
-    const item = Randomize(players, teams)
-    expect(item.length).toBe(4)
+const players = ["A", "B", "C", "D"];
+const teams = ["Braga", "Porto"];
+test("returns a match given a list", () => {
+  const items = Randomize(players, teams);
+  expect(items.length).toBe(4);
+  console.log(items[0]);
 });
